d37cccb5ece209bc8ce3cb08ad209b2dbfcde6dd
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-12-04 23:09:45 +0300

    Created a program into folder Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+void CreateCustomArray(int num)
+{
+    int[] Ary = new int[num];
+
+    for (int i =0; i < Ary.Length; i++)
+    {
+        Ary[i] = new Random().Next(0,100);
+    }
+
+    Console.Write($"Сгенерирован произвольный массив из {num} элементов: ");
+     foreach (int i in Ary)
+    {
+        Console.Write($"{i} ");
+    }
+}
+
+Console.Write("Введите число элементов массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CreateCustomArray(number);
