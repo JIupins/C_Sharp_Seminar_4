ab2fc348225e6e4703d1731a2db8b09ca37543ec
AlekseyBaboshin <BaboshinAP@gmail.com>
2022-12-04 21:53:43 +0300

    Created a program into folder Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+int EnterNumber(string word)
+{
+    Console.Write($"Введите число - {word}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void RaisingNumberToPower(int bas, int deg)
+{
+    int count = deg;
+    int result = 1;
+
+    for ( ; count > 0; count--)
+    {
+        result *= bas;
+    }
+    Console.WriteLine($"{bas} в степени {deg} равно {result}");
+}
+
+int basis = EnterNumber("основание");
+int degree = EnterNumber("степень");
+
+RaisingNumberToPower(basis, degree);
